52143aea07ef2dc87f1e50a5fe22bcfb4e607632
tangyuan31412 <70282618+tangyuan31412@users.noreply.github.com>
2022-07-23 16:03:26 +0800

    Update right.rs

--- src/right.rs
+++ src/right.rs
@@ -1,12 +1,77 @@
-mod shapes;
+use std::f64::consts::PI;
 
-use shapes::{print_area, Circle, Rectangle, RightTriangle};
+#[derive(Debug)]
+pub struct Circle<T> {
+    pub r: T,
+}
+
+#[derive(Debug)]
+pub struct Triangle<T, U, V> {
+    pub a: T,
+    pub b: U,
+    pub c: V,
+}
+
+#[derive(Debug)]
+pub struct Square<T> {
+    pub a: T,
+}
+
+pub trait AreaCalculate {
+    fn area(&self) -> f64;
+}
+
+impl<T> AreaCalculate for Circle<T>
+where
+    T: Copy + Into<f64>,
+{
+    fn area(&self) -> f64 {
+        let r: f64 = self.r.into();
+        PI * r * r
+    }
+}
+
+impl<T> AreaCalculate for Square<T>
+where
+    T: Copy + Into<f64>,
+{
+    fn area(&self) -> f64 {
+        let a: f64 = self.a.into();
+        a * a
+    }
+}
+
+impl<T, U, V> AreaCalculate for Triangle<T, U, V>
+where
+    T: Copy + Into<f64>,
+    U: Copy + Into<f64>,
+    V: Copy + Into<f64>,
+{
+    fn area(&self) -> f64 {
+        let a: f64 = self.a.into();
+        let b: f64 = self.b.into();
+        let c: f64 = self.c.into();
+        let p = (a + b + c) / 2.0;
+
+        (p * (p - a) * (p - b) * (p - c)).sqrt()
+    }
+}
+
+pub fn area<T: AreaCalculate>(shape: &T) -> f64 {
+    shape.area()
+}
 
 fn main() {
-    let s1 = RightTriangle::new(3.0, 4.0);
-    let s2 = Rectangle::new(3.0, 4.0);
-    let s3 = Circle::new(5.0);
-    print_area(&s1);
-    print_area(&s2);
-    print_area(&s3);
+    let circle = Circle { r: 10u8 };
+    println!("{:#?} \n area {}\n", circle, area(&circle));
+
+    let triangle = Triangle {
+        a: 3u32,
+        b: 4.0f32,
+        c: 5.00000000000001f64,
+    };
+    println!("{:#?} \n area {}\n", triangle, area(&triangle));
+
+    let square = Square { a: 10u32 };
+    println!("{:#?} \n area {}\n", square, area(&square));
 }
